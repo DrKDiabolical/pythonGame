a1d4e08bd53ca89a475d8e6b6bdbb67a5040ff7e
Daniel Downes <danieldownescentennial@gmail.com>
2018-08-01 11:26:04 -0700

    Still on level generation, got one line to render correctly

--- main.py
+++ main.py
@@ -10,21 +10,22 @@ Surface = pygame.display.set_mode((WIDTH, HEIGHT)) # Create screen
 pygame.display.set_caption("Gridrunner") # Set caption
 clock = pygame.time.Clock() # Initialize clock
 
-x = WIDTH/2
-y = HEIGHT/2
+playerSpawn = [64, 64]
+x = playerSpawn[0]
+y = playerSpawn[1]
 
 # Empty = 0   Wall = 1   Player = 2   Enemy = 3
 # Coin = 4   Button = 5   Open Door = 6   Closed Door = 7
 
 # This list is the layout for the level (8x8 grid)
 level = [
- [1, 1, 1, 1, 1, 1, 1, 1 ],
- [1, 0, 0, 0, 0, 0, 3, 1 ],
+ [1, 0, 1, 0, 0, 1, 1, 1 ],
+ [1, 0, 0, 0, 0, 0, 0, 1 ],
  [1, 0, 0, 1, 1, 1, 1, 1 ],
  [1, 0, 0, 0, 0, 0, 0, 1 ],
  [1, 1, 1, 1, 1, 1, 0, 1 ],
  [1, 0, 0, 0, 0, 0, 0, 1 ],
- [1, 2, 0, 0, 0, 0, 0, 1 ],
+ [1, 0, 0, 0, 0, 0, 0, 1 ],
  [1, 1, 1, 1, 1, 1, 1, 1 ]
 ]
 
@@ -39,18 +40,32 @@ def spritesheet(sheet, rectangle, scaleTo):
     return image
 
 # Generates a level based on the level list
-def createLevel(level):
-    # Check size
-    gridSize = len(level[0])
-    gridPixel = 64
+def createLevel(level, gridSize, gridRes):
     levelGrid = []
     # Scan one gridspot | for sprite type
-    for y in range(gridSize):
-        for x in range(gridSize):
-            levelGrid.append(level[y][x])
-    # Place item and move to next one
-    print(levelGrid)
-    # Jump back once the gridmax has been reached
+    for i in range(gridSize):
+        for l in range(gridSize):
+            levelGrid.append(level[i][l])
+    # Place sprite and move to next one
+    gridX = 0
+    gridY = 0
+    for i in range(gridSize + 1):
+        for l in levelGrid:
+            if l == 0:
+                gridX += gridRes
+                if i == 8:
+                    gridY += gridRes
+                    i = 0
+                else:
+                    continue
+            elif l == 1:
+                Surface.blit(wall, (gridX, gridY))
+                gridX += gridRes
+                if i == 8:
+                    gridY += gridRes
+                    i = 0
+                else:
+                    continue
 
 # Creates the sprites from the provided spritesheet and stores them in individual surfaces
 ss = pygame.image.load("spritesheet.png").convert()
@@ -63,8 +78,6 @@ buttonClicked = spritesheet(ss, (4, 4, 4, 4), 64)
 doorOpen = spritesheet(ss, (0, 0, 4, 4), 64)
 doorClosed = spritesheet(ss, (4, 0, 4, 4), 64)
 
-createLevel(level)
-
 isPlaying = True
 while isPlaying:
     for event in pygame.event.get():
@@ -72,7 +85,9 @@ while isPlaying:
             isPlaying = False
 
     Surface.fill((0, 0, 0))
-    
+
+    createLevel(level, 8, 64)
+
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_UP]: y -= 3
     if pressed[pygame.K_DOWN]: y += 3
